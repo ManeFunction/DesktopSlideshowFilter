5eace51e53e1e25d852525abeacec0f03ecfa090
Mane Function <mfunc@outlook.com>
2020-08-15 19:20:57 +0300

    Added solution and readme

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using Microsoft.Win32;
+
+namespace DesktopFilter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // read registry key (it's binary)
+            byte[] wallpaperBytes = Registry.GetValue(@"HKEY_CURRENT_USER\Control Panel\Desktop", "TranscodedImageCache",0) as byte[];
+            if (wallpaperBytes == null)
+            {
+                return;
+            }
+
+            // convert bytes to text
+            const int shift = 24;
+            char[] sourceChars = Encoding.UTF8.GetChars(wallpaperBytes, shift, wallpaperBytes.Length - shift);
+
+            // trim empty chars at the end
+            int pathLength = sourceChars.Length - 1;
+            while (sourceChars[pathLength] == '\0')
+            {
+                pathLength--;
+            }
+            
+            // clean strange spaces between chars
+            bool copy = true;
+            char[] targetChars = new char[pathLength / 2 + 1];
+            for (int n = 0, m = 0; n <= pathLength; n++)
+            {
+                if (copy)
+                {
+                    targetChars[m] = sourceChars[n];
+                }
+                else
+                {
+                    m++;
+                }
+                copy = !copy;
+            }
+            string wallpaperPath = new string(targetChars);
+
+            // delete the file
+            if (File.Exists(wallpaperPath))
+            {
+                string isDebug = string.Empty;
+#if DEBUG
+                isDebug = " (debug)";
+#endif
+                Console.WriteLine($"You are about to delete{isDebug}: {wallpaperPath} (Y/N)?");
+                if (Console.ReadKey().Key == ConsoleKey.Y)
+                {
+#if DEBUG
+                        Console.WriteLine("DELETED!");
+                        Console.ReadKey();
+#else
+                        File.Delete(wallpaperPath);
+#endif
+                }
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-DesktopSlideshowFilter
+**desktop-slideshow-filter**
+
+Very often, I set for my desktop wallpaper vast image collections with hundreds and thousands of pictures. And often it's hard as hell to find one picture to delete that pop up on your screen, and you don't like it. So I made a small console app for Windows that can take an actual picture's name from a registry for you so you can move it or even better, just delete it with a simple click!
+
+There's even a .reg file to desktop context menu integration!
+**Don't forget to change a path to the .exe in it before adding!**
+
+Also, I'm working on a Mac computer, so I've made me a script with similar features too.
+
+Hope it'll be helpful for someone. Feel free to use and modify :)
+
+**Built version for Windows, .reg file for context menu integration and workaround .sh script for the Mac can be found [HERE](https://github.com/ManeFunction/desktop-slideshow-filter/releases)!**
